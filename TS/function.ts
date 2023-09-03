6f6e894acd84a0a7e7bc54bb321996794d66bedd
Henrique Reimão <59811181+HenriqueReimao@users.noreply.github.com>
2023-09-03 19:39:04 +0000

    tipo e chamadas de função no código TypeScript

--- TS/function.ts
+++ TS/function.ts
@@ -1,7 +1,13 @@
-function functionSoma(x: number, y: number): number{
-return x + y;
+function functionSoma(x: number, y: number): number {
+    return x + y;
 }
 
-let soma: number = functionSoma(6,7);
+function functionSomatext(tx: string, tx0: string): string {
+    return tx + tx0;
+}
+
+let somatx: string = functionSomatext("hello ", "world"); // Correção: as strings devem estar entre aspas
+let soma: number = functionSoma(6, 7);
 
 console.log(soma);
+console.log(somatx); 
