9b5d3ac2590060d4e6832138342cd3ca001fe5a2
Henrique Reimão <59811181+HenriqueReimao@users.noreply.github.com>
2023-09-03 18:55:42 +0000

    Arrays

---
+++ TS/Arrays.ts
@@ -0,0 +1,10 @@
+
+//Arrays variavel: tipo [] = ["texto", "texto2"];
+
+let variavel: string[] = ["texto", "Texto0"];
+
+
+let dados: Array<string> = ["texto", "Texto0"];
+
+// Array com tipos diferentes 
+let tiposDiferentes: Array<string | number> = ["texto e", 0];
