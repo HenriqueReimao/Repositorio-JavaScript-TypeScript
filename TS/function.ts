0fd9d719bc272ec84aa7cbc6467d27f5067c2e32
Henrique Reimão <59811181+HenriqueReimao@users.noreply.github.com>
2023-09-03 19:44:19 +0000

    - Função com dados difrentes no código TypeScript

--- TS/function.ts
+++ TS/function.ts
@@ -2,11 +2,11 @@ function functionSoma(x: number, y: number): number {
     return x + y;
 }
 
-function functionSomatext(tx: string, tx0: string): string {
+function functionSomatext(tx: string, tx0: number): string | number {
     return tx + tx0;
 }
 
-let somatx: string = functionSomatext("hello ", "world"); // Correção: as strings devem estar entre aspas
+let somatx: string | number = functionSomatext("hello ", 9); // Correção: as strings devem estar entre aspas
 let soma: number = functionSoma(6, 7);
 
 console.log(soma);
