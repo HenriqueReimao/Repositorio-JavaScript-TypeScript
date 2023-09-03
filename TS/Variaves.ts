a20a722f7607ef9356eac16cc7d0b0cbb61f5a39
Henrique Reimão <59811181+HenriqueReimao@users.noreply.github.com>
2023-09-03 18:39:54 +0000

    - Variaves com TapeScritp

---
+++ TS/Variaves.ts
@@ -0,0 +1,21 @@
+/**
+ * Variaveis
+ */
+
+//Tipo Primitivos: Bollean, Number, String 
+let ligando: boolean = false;
+let nome: string = "Henrique";
+let idade: number = 26;
+let altura: number = 1.90;
+// é uma trava ou seja só podemos passar parametros com tipo
+
+//tipos especiais: null, undefined
+let nulo: null = null;
+let indefinido: undefined = undefined;
+
+//Tipos Abrangentes: andy, void
+// o retorno dela é vazio
+// let retorno:void = void;
+
+// retorna qualquer dado trasformando o ty em js 
+let retornoView:any = true;
